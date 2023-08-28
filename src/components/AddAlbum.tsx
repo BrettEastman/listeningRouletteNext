ca1daa7be8f0889cc4a991f88bf7de196fc8669e
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-08-27 19:03:59 -0700

    Refactor Home page to be placeholder for auth login forms. Move main page to src/home/page.tsx

---
+++ src/components/AddAlbum.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useState } from "react";
+import styled from "styled-components";
+
+export default function AddAlbum({ handleAlbum }: any) {
+  const [album, setAlbum] = useState("");
+  const [artist, setArtist] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const data = {
+      album: album,
+      artist: artist,
+    };
+    handleAlbum(data);
+  };
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Album:
+          <input
+            type="text"
+            name="album"
+            onChange={(e) => setAlbum(e.target.value)}
+          />
+        </label>
+        <label>
+          Artist:
+          <input
+            type="text"
+            name="artist"
+            onChange={(e) => setArtist(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
